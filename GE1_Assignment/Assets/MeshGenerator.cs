74314a4c79bd7522fb82825e95e92d822b872b66
hugihu <c17316046@mytudublin.ie>
2021-12-02 18:36:37 +0000

    Mesh Render 20 x 20

--- GE1_Assignment/Assets/MeshGenerator.cs
+++ GE1_Assignment/Assets/MeshGenerator.cs
@@ -9,6 +9,9 @@ public class MeshGenerator : MonoBehaviour
 
     Vector3[] vertices;
     int[] triangles;
+
+    public int xSize = 20;
+    public int zSize = 20;
     
     // Start is called before the first frame update
     void Start()
@@ -22,19 +25,25 @@ public class MeshGenerator : MonoBehaviour
 
     void CreateShape ()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3 (0,0,0),
-            new Vector3 (0,0,1),
-            new Vector3 (1,0,0),
-            new Vector3 (1,0,1)
-        };
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
-        triangles = new int[]
+
+        for (int i = 0, z = 0; z < zSize; z++)
         {
-            0, 1, 2,
-            1, 3, 2
-        };
+            for (int x = 0; x < xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        triangles = new int[6];
+        triangles[0] = 0;
+        triangles[1] = xSize + 1;
+        triangles[2] = 1;
+        triangles[3] = 1;
+        triangles[4] = xSize + 1;
+        triangles[5] = xSize + 2;
     }
     
     void UpdateMesh()
@@ -45,5 +54,17 @@ public class MeshGenerator : MonoBehaviour
         mesh.triangles = triangles;
 
         mesh.RecalculateNormals();
+    }
+
+    void onDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+
     }
 }
