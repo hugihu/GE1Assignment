baf07297b6b421a17f1fb1ee4dbb0c6657ef0471
hugihu <c17316046@mytudublin.ie>
2021-12-02 19:18:54 +0000

    Render Commit

--- GE1_Assignment/Assets/MeshGenerator.cs
+++ GE1_Assignment/Assets/MeshGenerator.cs
@@ -37,13 +37,25 @@ public class MeshGenerator : MonoBehaviour
             }
         }
 
-        triangles = new int[6];
-        triangles[0] = 0;
-        triangles[1] = xSize + 1;
-        triangles[2] = 1;
-        triangles[3] = 1;
-        triangles[4] = xSize + 1;
-        triangles[5] = xSize + 2;
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
+
+        for (int x = 0; x < xSize; x++)
+        {
+           
+            triangles[tris + 0] = vert + 0;
+            triangles[tris + 1] = vert + xSize + 1;
+            triangles[tris + 2] = vert + 1;
+            triangles[tris + 3] = vert + 1;
+            triangles[tris + 4] = vert + xSize + 1;
+            triangles[tris + 5] = vert + xSize + 2;   
+
+            vert++;
+            tris += 6;
+        }
+
+      
     }
     
     void UpdateMesh()
