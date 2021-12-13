1ee0306dfe44bc31877f10a2a0a944b58b79d808
hugihu <c17316046@mytudublin.ie>
2021-12-13 18:41:40 +0000

    height

--- GE1_Assignment/Assets/TileGeneration.cs
+++ GE1_Assignment/Assets/TileGeneration.cs
@@ -2,18 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class TerrainType {
-  public string name;
-  public float height;
-  public Color color;
-}
 
 public class TileGeneration : MonoBehaviour {
 
     [SerializeField]
     private TerrainType[] terrainTypes;
-    
+
     [SerializeField]
     NoiseMapGeneration noiseMapGeneration;
 
@@ -29,6 +23,16 @@ public class TileGeneration : MonoBehaviour {
     [SerializeField]
     private float mapScale;
 
+    [SerializeField]
+    private float heightMultiplier;
+
+    [System.Serializable]
+    public class TerrainType {
+        public string name;
+        public float height;
+        public Color color;
+    }
+
     void Start() {
         GenerateTile();
     }
@@ -41,10 +45,21 @@ public class TileGeneration : MonoBehaviour {
         int tileWidth = tileDepth;
 
         // calculate the offsets based on the tile position
-        float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap (tileDepth, tileWidth, this.mapScale);
+        float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap(tileDepth, tileWidth, this.mapScale);
         // generate a heightMap using noise
-        Texture2D tileTexture = BuildTexture (heightMap);
+        Texture2D tileTexture = BuildTexture(heightMap);
         this.tileRenderer.material.mainTexture = tileTexture;
+    }
+
+    TerrainType ChooseTerrainType(float height) {
+    // for each terrain type, check if the height is lower than the one for the terrain type
+        foreach (TerrainType terrainType in terrainTypes) {
+        // return the first terrain type whose height is higher than the generated one
+        if (height < terrainType.height) {
+            return terrainType;
+        }
+    }
+    return terrainTypes [terrainTypes.Length - 1];
     }
 
     private Texture2D BuildTexture(float[,] heightMap) {
@@ -52,21 +67,52 @@ public class TileGeneration : MonoBehaviour {
         int tileWidth = heightMap.GetLength(1);
 
         Color[] colorMap = new Color[tileDepth * tileWidth];
+
         for (int zIndex = 0; zIndex < tileDepth; zIndex++) {
             for (int xIndex = 0; xIndex < tileWidth; xIndex++) {
                 // transform the 2D map index is an Array index
-                int colorIndex = zIndex * tileWidth+ xIndex;
-                float height= heightMap[zIndex, xIndex];
-                // assign as color a shade of grey proportional to the height value
-                colorMap [colorIndex] = Color.Lerp (Color.black, Color.white, height);
+                int colorIndex = zIndex * tileWidth + xIndex;
+                float height = heightMap [zIndex, xIndex];
+                // choose a terrain type according to the height value
+                TerrainType terrainType = ChooseTerrainType (height);
+                // assign the color according to the terrain type
+                colorMap[colorIndex] = terrainType.color;
             }
         }
-         // create a new texture and set its pixel colors
+
+        // create a new texture and set its pixel colors
         Texture2D tileTexture = new Texture2D (tileWidth, tileDepth);
         tileTexture.wrapMode = TextureWrapMode.Clamp;
         tileTexture.SetPixels (colorMap);
         tileTexture.Apply ();
         return tileTexture;
     }
-    
+
+    private void UpdateMeshVertices(float[,] heightMap) {
+        int tileDepth = heightMap.GetLength (0);
+        int tileWidth = heightMap.GetLength (1);
+        Vector3[] meshVertices = this.meshFilter.mesh.vertices;
+        // iterate through all the heightMap coordinates, updating the vertex index
+        int vertexIndex = 0;
+
+        for (int zIndex = 0; zIndex < tileDepth; zIndex++) {
+            for (int xIndex = 0; xIndex < tileWidth; xIndex++) {
+                float height = heightMap [zIndex, xIndex];
+                Vector3 vertex = meshVertices [vertexIndex];
+                // change the vertex Y coordinate, proportional to the height value
+                meshVertices[vertexIndex] = new Vector3(vertex.x, height * this.heightMultiplier, vertex.z);
+                vertexIndex++;
+            }
+        }
+
+        // update the vertices in the mesh and update its properties
+        this.meshFilter.mesh.vertices = meshVertices;
+        this.meshFilter.mesh.RecalculateBounds ();
+        this.meshFilter.mesh.RecalculateNormals ();
+        // update the mesh collider
+        this.meshCollider.sharedMesh = this.meshFilter.mesh;
+    }
+
 }
+        
+
