accdecdc9bd714083c885a3b479bbbd95e38220c
hugihu <c17316046@mytudublin.ie>
2021-12-13 16:04:34 +0000

    Tile Gen

--- GE1_Assignment/Assets/NoiseMapGeneration.cs
+++ GE1_Assignment/Assets/NoiseMapGeneration.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoiseMapGeneration : MonoBehaviour
-{
-    public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale){
-        float[,] noisemap = new float[mapDepth, mapWidth];
+public class NoiseMapGeneration : MonoBehaviour{
+  public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale){
+    // create an empty noise map with the mapDepth and mapWidth coordinates
+    float[,] noisemap = new float[mapDepth, mapWidth];
+    
+    for (int zIndex = 0; zIndex < mapDepth; zIndex ++) {
+        for (int xIndex = 0; xIndex < mapWidth; xIndex ++){
+            // calculate sample indices based on the coordinates and the scale
+            float sampleX = xIndex / scale;
+            float sampleZ = zIndex / scale;
 
-        for (int zIndex = 0; zIndex < mapDepth; zIndex ++){
-            for (int xIndex = 0; xIndex < mapWidth; xIndex ++){
-                float sampleX = xIndex / scale;
-                float sampleZ = zIndex / scale;
+            // generate noise value using PerlinNoise
 
-                float noise = Mathf.PerlinNoise (sampleX, sampleZ);
-
-                noisemap [zIndex, xIndex] = noise;
-            }
-        }
-
-        return noisemap;
+            float noise = Mathf.PerlinNoise (sampleX, sampleZ);
+            noisemap [zIndex, xIndex] = noise;
+      }
     }
+    return noisemap;
+  }
 }
-
 
