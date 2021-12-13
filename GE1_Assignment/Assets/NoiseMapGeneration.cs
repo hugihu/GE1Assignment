eac666872e926b90c9a15bd9877b08bf7940cac8
hugihu <c17316046@mytudublin.ie>
2021-12-13 15:14:37 +0000

    Perlin and Tile Generation

---
+++ GE1_Assignment/Assets/NoiseMapGeneration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMapGeneration : MonoBehaviour
+{
+    public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale){
+        float[,] noisemap = new float[mapDepth, mapWidth];
+
+        for (int zIndex = 0; zIndex < mapDepth; zIndex ++){
+            for (int xIndex = 0; xIndex < mapWidth; xIndex ++){
+                float sampleX = xIndex / scale;
+                float sampleZ = zIndex / scale;
+
+                float noise = Mathf.PerlinNoise (sampleX, sampleZ);
+
+                noisemap [zIndex, xIndex] = noise;
+            }
+        }
+
+        return noisemap;
+    }
+}
+
+
