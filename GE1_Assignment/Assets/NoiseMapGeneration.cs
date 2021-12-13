6304e47ef112d63674ddfe88e0fc128631549d3c
hugihu <c17316046@mytudublin.ie>
2021-12-13 18:41:26 +0000

    Height

--- GE1_Assignment/Assets/NoiseMapGeneration.cs
+++ GE1_Assignment/Assets/NoiseMapGeneration.cs
@@ -6,8 +6,8 @@ public class NoiseMapGeneration : MonoBehaviour{
   public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale){
     // create an empty noise map with the mapDepth and mapWidth coordinates
     float[,] noisemap = new float[mapDepth, mapWidth];
-    
-    for (int zIndex = 0; zIndex < mapDepth; zIndex ++) {
+
+    for (int zIndex = 0; zIndex < mapDepth; zIndex ++){
         for (int xIndex = 0; xIndex < mapWidth; xIndex ++){
             // calculate sample indices based on the coordinates and the scale
             float sampleX = xIndex / scale;
