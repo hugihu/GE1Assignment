aeb6c505f0837b8a82e30ff9d47046cde869bc36
Hugh O'Carroll-Macri <c17316046@mytudublin.ie>
2021-12-14 21:00:19 +0000

    Movement + Gravity imp

--- GE1_Assignment/Assets/ThirdPersonMovement.cs
+++ GE1_Assignment/Assets/ThirdPersonMovement.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
+    Vector3 moveVector;
     
 
     public float speed = 6f;
 
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVeloicity;
 
-    // Update is called once per frame
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded == false)
+        {
+            moveVector +=Physics.gravity;
+
+        }
+        controller.Move(moveVector * Time.deltaTime);
+
+        
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVeloicity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            controller.Move(direction * speed * Time.deltaTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
